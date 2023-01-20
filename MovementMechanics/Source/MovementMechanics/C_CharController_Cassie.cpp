fd110ad9142d171f4d9359393d40862a97bdf693
Marcio Ferreira <marcio2.ferreira@live.uwe.ac.uk>
2023-01-20 02:27:35 +0000

    Initial Control
    Initial movement inputs and area.
    Player still floats.
    Problems with Visual Studio seem to persist but usage of Clion can be used to circumvent

--- MovementMechanics/Source/MovementMechanics/C_CharController_Cassie.cpp
+++ MovementMechanics/Source/MovementMechanics/C_CharController_Cassie.cpp
@@ -9,13 +9,14 @@ AC_CharController_Cassie::AC_CharController_Cassie()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
+	Camera->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
 void AC_CharController_Cassie::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
@@ -30,5 +31,30 @@ void AC_CharController_Cassie::SetupPlayerInputComponent(UInputComponent* Player
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+    PlayerInputComponent->BindAxis(TEXT("Look Horizontal"),this,&AFirstPersonPlayerCharacter::LookHorizontal);
+	PlayerInputComponent->BindAxis(TEXT("Look Vertical"),this,&AFirstPersonPlayerCharacter::LookVertical);
+	PlayerInputComponent->BindAxis(TEXT("Movement Forward"),this,&AFirstPersonPlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("Movement Sideway"),this,&AFirstPersonPlayerCharacter::MoveSideway);
+	PlayerInputComponent->BindAction(TEXT("Jump"),IE_Pressed,this,&ACharacter::Jump);
+
+void AC_CharController_Cassie::LookHorizontal(float axis_value)
+{
+	this->AddControllerYawInput(axis_value);
+}
+void AC_CharController_Cassie::LookVertical(float axis_value)
+{
+	this->AddControllerPitchInput(axis_value);
+}
+void AC_CharController_Cassie::MoveSideway(float axis_value)
+{
+	this->AddMovementInput(this->GetActorForwardVector() * axis_value)
+}
+void AC_CharController_Cassie::MoveForward(float axis_value)
+{
+	if (axis_value > 0)
+	{
+		(this->GetActorRightVector() * axis_value * character_speed);
+	}
+	this->AddMovementInput();
 }
 
--- MovementMechanics/Source/MovementMechanics/C_CharController_Cassie.h
+++ MovementMechanics/Source/MovementMechanics/C_CharController_Cassie.h
@@ -5,19 +5,30 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "C_CharController_Cassie.generated.h"
+#include "Camera/CameraComponent.h"
 
 UCLASS()
 class MOVEMENTMECHANICS_API AC_CharController_Cassie : public ACharacter
 {
 	GENERATED_BODY()
-
 public:
 	// Sets default values for this character's properties
 	AC_CharController_Cassie();
 
+	float character_speed = 1.0f;
+	float strafe_multiplier = 0.8f;
+	float back_multiplier = 0.6f;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	void LookHorizontal(float axis_value);
+	void LookVertical(float axis_value);
+	void MoveSideway(float axis_value);
+	void MoveForward(float axis_value);
+
+	UPROPERTY(EditAnywhere, Category = "Components")
+		UCameraComponent* Camera;
 
 public:	
 	// Called every frame
